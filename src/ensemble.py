a2a8aad699e1cf4f188f3e03caef1940ed3474fd
Asha Raghav <z5363204@ad.unsw.edu.au>
2023-07-31 17:53:26 +1000

    finished ensemble code (without eda)

--- src/ensemble.py
+++ src/ensemble.py
@@ -25,28 +25,43 @@ def read_sub(fp, weight):
             .with_columns(pl.col('aid').cast(pl.UInt32)).with_columns(pl.col('v').cast(pl.UInt8))
     )
 
+def perform_ensemble(model_predictions):
+    '''
+    Carries out voter ensemble
+
+    Creates df based on predictions from the given models
+
+    Params:
+        model_predictions (list): contains the predictions from given models
+
+    Returns:
+        final_predictions (df): ensemble predictions after voting ensemble
+    '''
+    model_predictions = model_predictions[0].join(model_predictions[1], 
+                                                how='outer', 
+                                                on=['session_type', 
+                                                    'aid']).join(model_predictions[2], 
+                                                                how='outer', on=['session_type', 'aid'], 
+                                                                suffix='r2')
+    # Fill gaps (values that are currently null) 
+    model_predictions = (model_predictions
+        .fill_null(0).with_columns((pl.col('v') + pl.col('v_right') + pl.col('vr2')).alias('sum_votes'))
+        .drop(['v', 'v_right', 'vr2']).sort(by='sum_votes').reverse()
+    )
+    # Format
+    final_predictions = model_predictions.groupby('session_type').agg([
+        pl.col('aid').head(20).alias('labels')
+    ])
+    # Casting the numbers to improve speed
+    final_predictions = final_predictions.with_columns(pl.col('labels').cast(pl.List(pl.Utf8)).list.join(' '))
+    return final_predictions
+
 # Store the prediction filepaths
 prediction_fps = ['./../ensemble-submissions/w2v_predictions.csv', './../ensemble-submissions/xgb_predictions.csv',
          './../ensemble-submissions/covis_predictions.csv']
 # Applying greater weights to better models 
 model_predictions = [read_sub(fp, weight_val) for fp, weight_val in zip(prediction_fps, [0.2, 0.3, 0.5])]
-model_predictions = model_predictions[0].join(model_predictions[1], 
-                                              how='outer', 
-                                              on=['session_type', 
-                                                  'aid']).join(model_predictions[2], 
-                                                               how='outer', on=['session_type', 'aid'], 
-                                                               suffix='r2')
-print(model_predictions.head())
-# Fill gaps (values that are currently null) 
-model_predictions = (model_predictions
-    .fill_null(0).with_columns((pl.col('v') + pl.col('v_right') + pl.col('vr2')).alias('sum_votes'))
-    .drop(['v', 'v_right', 'vr2']).sort(by='sum_votes').reverse()
-)
-# Format
-final_predictions = model_predictions.groupby('session_type').agg([
-    pl.col('aid').head(20).alias('labels')
-])
-# Casting the numbers to improve speed
-final_predictions = final_predictions.with_columns(pl.col('labels').cast(pl.List(pl.Utf8)).list.join(' '))
+# Perform the voter ensemble method
+final_predictions = perform_ensemble(model_predictions)
 # Place final predictions in submission file
 final_predictions.write_csv('submission.csv')
